d3511cb3bc5e69679218105c25e24b741e8aefa0
elliot-bordrez <90181718+elliot-bordrez@users.noreply.github.com>
2023-06-07 11:07:19 +0200

    allumerled.ino

--- Code_arduino/allumerled.ino
+++ Code_arduino/allumerled.ino
@@ -1,54 +1,93 @@
-#include <Ethernet.h> // Inclut la bibliothèque Ethernet
 #include <SPI.h>
-const int led1Pin = 8; // Broche de la LED 1
+#include <Ethernet.h>
+#include <ArduinoJson.h>
 
-byte mac[] = { 0xA8, 0x61, 0x0A, 0xAE, 0x75, 0x24 }; // Adresse MAC de l'Arduino
-IPAddress ip(192,168,64,6); // Adresse IP 
+const int ledPin = 8; // Broche de la LED
+int ledState = LOW;    // État actuel de la LED (initialisé à LOW)
 
+byte mac[] = {0xA8, 0x61, 0x0A, 0xAE, 0x75, 0x24};
+IPAddress ip(192, 168, 64, 5); // Adresse IP du serveur web de l'Arduino sur le réseau local
+EthernetServer server(80); // Crée un serveur web sur le port standard 80
 
-EthernetServer server(80); // Crée un serveur TCP sur le port 80
-void setup() {
-  // Configure les broches de sortie et initialise l'Ethernet et le serveur
-
-  pinMode(led1Pin, OUTPUT);
-  Ethernet.begin(mac, ip);
-
-  
-  server.begin();
-
-
-  Serial.begin(9600);
-
-  
+void setup()
+{
+    Serial.begin(9600);
+    Ethernet.begin(mac, ip);
+    server.begin();
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, ledState);
 }
 
-void loop() {
-  
-  // Ecoute les connexions entrantes et traite les commandes reçues
-  EthernetClient client = server.available();
-  
-  Serial.println(client);
-  while (client) 
-  { // Si une connexion est disponible
-    // lisez les données envoyées par le client
-    String commande = client.readStringUntil('\r');
+void loop()
+{
+    handleClientRequests();
+}
 
-    if (commande == "allumer") {
-      
-      digitalWrite(led1Pin, HIGH);
+void handleClientRequests()
+{
+    EthernetClient client = server.available();
 
-     } else if (commande == "eteindre")
+    if (client)
+    {
+        boolean currentLineIsBlank = true;
+        String request = "";
 
-      
+        while (client.connected())
+        {
+            if (client.available())
+            {
+                char c = client.read();
+                request += c;
 
+                if (c == '\n' && currentLineIsBlank)
+                {
+                    // Analyser la requête
+                    if (request.indexOf("GET /?led=1") != -1)
+                    {
+                        ledState = HIGH;
+                        digitalWrite(ledPin, ledState);
+                    }
+                    else if (request.indexOf("GET /?led=0") != -1)
+                    {
+                        ledState = LOW;
+                        digitalWrite(ledPin, ledState);
+                    }
 
-    client.stop(); // Ferme la connexion avec le client
-   }
+                    // Envoyer la réponse HTTP
+                    sendHttpResponse(client);
 
-    //String commande = "reculer";
+                    break;
+                }
 
-    //Serial.println(commande);
-      
-  
-  
+                if (c == '\n')
+                    currentLineIsBlank = true;
+                else if (c != '\r')
+                    currentLineIsBlank = false;
+            }
+        }
+
+        delay(500);
+        client.stop();
+    }
+}
+
+void sendHttpResponse(EthernetClient client)
+{
+    client.println("HTTP/1.1 200 OK");
+    client.println("Access-Control-Allow-Origin: *");
+    client.println("Content-Type: text/html");
+    client.println("Connection: close");
+    client.println();
+
+    client.println("<html><body><h1>Contrôle de la LED</h1>");
+    client.println("<p>État de la LED : ");
+    client.println(ledState == HIGH ? "Allumée" : "Éteinte");
+    client.println("</p>");
+
+    client.println("<form>");
+    client.println("<button type='submit' name='led' value='1'>Allumer</button>");
+    client.println("<button type='submit' name='led' value='0'>Éteindre</button>");
+    client.println("</form>");
+
+    client.println("</body></html>");
 }
